9413d1e9085dbf5ebdc1aaded3ae4595eea1a77b
Guille <guillermobernallou@gmail.com>
2025-11-26 15:07:56 +0100

    feat: add stying to about page

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,7 +1,14 @@
+import styles from "./about.module.css";
+
 export default function AboutPage() {
   return (
-    <section>
-      <a href="">go back</a>
+    <section className={styles.root}>
+      <article>
+        <p>asdhakjsdhaksdhaksdagsdadsgakjsdgaksdjg</p>
+        <p>asdhakjsdhaksdhaksdagsdadsgakjsdgaksdjg</p>
+        <p>asdhakjsdhaksdhaksdagsdadsgakjsdgaksdjg</p>
+      </article>
+      <a href="/">go back</a>
     </section>
   );
 }
